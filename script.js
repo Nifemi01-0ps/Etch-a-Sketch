966bc5bf0e03f1bbd3d8c942c38c4c94e676eacf
Nifemi01 <raphaeledafesnr@gmail.com>
2025-04-21 14:08:27 +0100

    create a loop and add an event listner to the resizn button

--- script.js
+++ script.js
@@ -6,4 +6,27 @@ const container = document.querySelector("#container");
 function createSquare(size) {
     container.innerHTML = "";
     const squareSize = 512 / size;
+
+    // for loop to create square
+    for (let i = 0; i < size * size; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        container.appendChild(square);
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+        square.style.boxShadow = "border-box";
+        square.style.border = "1px solid #ccc"
+        square.addEventListener("mouseenter", () => {
+            square.style.backgroundColor = "black";
+        })
+    }
 }
+createSquare(16);
+
+resizeBtn.addEventListener("click", () => {
+    let value = prompt("Enter a value between 1 and 100");
+    if (value < 1 || value > 100) {
+        alert("Enter a value between 1 and 100");
+    }
+    createSquare(value);
+})
