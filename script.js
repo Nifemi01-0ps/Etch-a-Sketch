ec37488fb4fa5a85afcf867ba3ed1ccd656ae6ce
Nifemi01 <raphaeledafesnr@gmail.com>
2025-04-21 13:48:22 +0100

    craeate a function to create square for etch a sketch

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 // Reference html element
 const resizeBtn = document.querySelector("#resizeBtn");
 const container = document.querySelector("#container");
+
+// Create Square Function
+function createSquare(size) {
+    container.innerHTML = "";
+    const squareSize = 512 / size;
+}
